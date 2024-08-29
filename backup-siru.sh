be3a5271948979a62678c6d454124b48998d43ce
Jukka Villstedt <jukka.villstedt@iki.fi>
2024-08-29 08:34:20 +0300

    add backup scripts

---
+++ backup-siru.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+readonly SOURCE_DIR="/Users/jukka/sirun-google-drive"
+readonly BACKUP_DIR="/Volumes/Backup_3_1/google-drive-backup/siru"
+readonly DATETIME="$(date '+%Y-%m-%d_%H-%M-%S')"
+readonly BACKUP_PATH="${BACKUP_DIR}/${DATETIME}"
+readonly LATEST_LINK="${BACKUP_DIR}/latest"
+
+cp -Rl "${LATEST_LINK}/" "${BACKUP_PATH}"
+
+rsync -av --delete "${SOURCE_DIR}/" "${BACKUP_PATH}"
+
+rm -rf "${LATEST_LINK}"
+ln -s "${DATETIME}" "${LATEST_LINK}"
